896cf941329cdcef9a5253839da24fc5b4178454
kira <kr.214.ia@gmail.com>
2025-07-14 18:17:52 +0900

    Bullet

---
+++ Assets/Script/BulletMove.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour
+{
+    [SerializeField] private float bulletSpeed;
+    [SerializeField] private float lifeTime;
+    [SerializeField] private GameObject bulletPrefab;
+
+    private GameObject bullet;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            bullet = Instantiate(bulletPrefab, transform.position, bulletPrefab.transform.rotation);
+
+            rb = bullet.GetComponent<Rigidbody>();
+            rb.AddForce(new Vector3(0, bulletSpeed, 0));
+
+            Destroy(bullet, lifeTime);
+        }
+
+    }
+}
